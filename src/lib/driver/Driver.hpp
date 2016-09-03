dc2a83d026ac6a9f5bc3cf29f8d2ce9f34b11342
Piotr Srebniak <piotr.srebniak@gmail.com>
2016-09-03 19:05:55 +0200

    poprawiony cmake do automatycznego testowania

--- src/lib/driver/Driver.hpp
+++ src/lib/driver/Driver.hpp
@@ -15,17 +15,17 @@ public:
    Driver() = default;
 
    virtual ~Driver();
-   
-   /** 
+
+   /**
     * parse - parse from a file
     * @param filename - valid string with input file
     */
-   void parse( const char * const filename );
-   /** 
+   int parse( const char * const filename );
+   /**
     * parse - parse from a c++ input stream
     * @param is - std::istream&, valid input stream
     */
-   void parse( std::istream &iss );
+   int parse( std::istream &iss );
 
    void add_upper();
    void add_lower();
@@ -36,7 +36,7 @@ public:
    std::ostream& print(std::ostream &stream);
 private:
 
-   void parse_helper( std::istream &stream );
+   int parse_helper( std::istream &stream );
 
    std::size_t  chars      = 0;
    std::size_t  words      = 0;
@@ -45,7 +45,7 @@ private:
    std::size_t  lowercase  = 0;
    CSSP::Parser  *parser  = nullptr;
    CSSP::Scanner *scanner = nullptr;
-   
+
    const std::string red   = "\033[1;31m";
    const std::string blue  = "\033[1;36m";
    const std::string norm  = "\033[0m";
