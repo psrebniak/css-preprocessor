bc9dc102fd9cffe29afe52f97f5d8fdf35e3d16f
Piotr Srebniak <piotr.srebniak@gmail.com>
2016-11-14 21:03:47 +0100

    setup generator

--- src/lib/generator/Generator.cpp
+++ src/lib/generator/Generator.cpp
@@ -1,20 +1,30 @@
 #include "Generator.hpp"
 
 long CSSP::Generator::generateOutput(std::ostream &ostream) {
+
+    std::cout << "checking stack " << std::endl;
+
     while(!this->stack.empty()) {
+        std::cout << "before gettings locals" << std::endl;
         NodeVectorType::const_iterator iterator = this->stack.top().first;
         NodeVectorType nodes = this->stack.top().second;
 
+        std::cout << "before check iterator is end()" << std::endl;
+
         if (iterator == nodes.end()) {
+            std::cout << "before pop" << std::endl;
             this->stack.pop();
+            std::cout << "after pop" << std::endl;
             continue;
         }
 
+        std::cout << "before write out node type" << std::endl;
         std::cout << (*iterator)->getNodeType() << std::endl;
-
-        ++this->stack.top().first;
+        std::cout << "after write out node type" << std::endl;
+        this->stack.top().first++;
+        std::cout << "after increment iterator" << std::endl;
     }
-
+    std::cout << "end" << std::endl;
     return 0;
 }
 
