12eed23e685948f0ab17354517bbe8aa82e9c575
Piotr Srebniak <piotr.srebniak@gmail.com>
2016-11-14 19:54:42 +0100

    add generator and types

---
+++ src/lib/generator/Generator.hpp
@@ -0,0 +1,14 @@
+//
+// Created by piotrek on 14.11.16.
+//
+
+#ifndef CSSPREPROCESSOR_GENERATOR_HPP
+#define CSSPREPROCESSOR_GENERATOR_HPP
+
+
+class Generator {
+
+};
+
+
+#endif //CSSPREPROCESSOR_GENERATOR_HPP
